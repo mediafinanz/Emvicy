3aa78568cc215d3448cacaa549f96a7651148cef
guido <guido.ueffing@mediafinanz.de>
2025-01-13 10:33:38 +0100

    update

--- application/smartyPlugins/modifier.filesize.php
+++ application/smartyPlugins/modifier.filesize.php
@@ -1,17 +1,20 @@
 <?php
 
 /**
- * @package Emvicy
- * @copyright ueffing.net
- * @author Guido K.B.W. Üffing <emvicy@ueffing.net>
- * @license GNU GENERAL PUBLIC LICENSE Version 3. See application/doc/COPYING
- * @param $Byte
- * @param $iDecimals
+ * @param string $sByte
+ * @param int    $iDecimals
  * @return string
+ * @license   GNU GENERAL PUBLIC LICENSE Version 3. See application/doc/COPYING
+ * @package   Emvicy
+ * @copyright ueffing.net
+ * @author    Guido K.B.W. Üffing <emvicy@ueffing.net>
  */
-function smarty_modifier_filesize($Byte, $iDecimals = 2)
+function smarty_modifier_filesize(string $sByte, int $iDecimals = 2)
 {
     $sKey = 'BKMGTP';
-    $dFactor = floor((strlen($Byte) - 1) / 3);
-    return sprintf("%.{$iDecimals}f", $Byte / pow(1024, $dFactor)) . @$sKey[$dFactor];
+    $fFactor = (int) floor((strlen($sByte) - 1) / 3);
+    return sprintf(
+       "%.{$iDecimals}f",
+       $sByte / pow(1024, $fFactor)
+   ) . @$sKey[$fFactor];
 }
